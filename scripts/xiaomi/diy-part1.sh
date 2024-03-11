06ece9e615ea937fa03d6be04e7ce19ecddda8b1
armin19900 <154201535@qq.com>
2024-03-11 11:32:47 +0800

    Update diy-part1.sh

--- scripts/xiaomi/diy-part1.sh
+++ scripts/xiaomi/diy-part1.sh
@@ -18,3 +18,10 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 ## echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 ## echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+
+src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages
+src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci
+
+./scripts/feeds clean
+./scripts/feeds update -a
+./scripts/feeds install -a
